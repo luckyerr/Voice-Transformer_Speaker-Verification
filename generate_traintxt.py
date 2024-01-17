3c617e68db61b8e45c347790cd46c0d7c1e8e1a7
why414 <100473931+why414@users.noreply.github.com>
2024-01-17 15:00:26 +0800

    Update generate_traintxt.py

--- generate_traintxt.py
+++ generate_traintxt.py
@@ -1,6 +1,6 @@
 import os
 
-for dirpath, dirnames, filenames in os.walk('C:/Users/why/Desktop/aaa/'):
-        a = dirpath.split('aaa/')[-1]
+for dirpath, dirnames, filenames in os.walk('path/data/'):
+        a = dirpath.split('data/')[-1]
         for filename in filenames:
-            print(a + " " + dirpath.split('aaa/')[-1] + str("/") + filename)
+            print(a + " " + dirpath.split('data/')[-1] + str("/") + filename)
